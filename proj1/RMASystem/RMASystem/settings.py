f4f5d70f95b067c6967ce64b02bd9942c9629e67
jefferyzj <zjxudanfeng@gmail.com>
2024-12-20 11:59:54 -0800

    finish npm build

--- proj1/RMASystem/RMASystem/settings.py
+++ proj1/RMASystem/RMASystem/settings.py
@@ -139,7 +139,11 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "product_management/static",
+    BASE_DIR / "static/frontend",
 ]
+
+# Ensure that Django collects static files into a single directory
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
